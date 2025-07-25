ee2c8c1eb6d6ce57f25a6e98ace6104353da1069
josehernadez04 <josehernandez03b@gmail.com>
2025-07-24 22:00:46 -0500

    v1.4.codigo completo arrays
    se le agrega todos los métodos vistos en clase 
    se le agrega el menú de usuario con un controlador de seguir o terminar
    se comentario cada linea de código

--- base_datos.py
+++ base_datos.py
@@ -9,36 +9,73 @@ class lista_numeros:
  # se crea un  metodo llamado guardar numero con el (append)
     def guardar_numero(self, dato_numero): #se le agrega un parametro
         self.lista_numero.append(dato_numero)
-        print("Lista actual:", self.lista_numero)
+        print("Numeros agregados a la lista:", self.lista_numero)
     
 # se crea el metodo nueva lista 
     def nueva_lista(self,numeros_nuevos):# aca se agregan un parametro
         self.lista_numero.extend(numeros_nuevos)# se le agregan esos dos numeros a la lista 
-        print( "lista actual ",self.lista_numero)# y aqui no las muestra actualizada ya agg los dos numeros
+        print("Se agrega nuevos numeros a la lista :",self.lista_numero)# y aqui no las muestra actualizada ya agg los dos numeros
          
 # se crea el metodo donde vamos a insertar un numero con una posicion especifica 
     def insertar_en_posicion(self, posicion,agg_numeros):# se le agrega dos parametros  
         self.lista_numero.insert(posicion,agg_numeros)# en la lista se van a insertar estos dos argumentos 
-        print("Lista actual:", self.lista_numero)# y por ultimo se muestra la lista actualizada
+        print(f"se agrego los numeros: {agg_numeros}, en la posicion:{posicion} ")# y por ultimo se muestra la lista actualizada
         
-# se crea el meto para borrar un numero de la lista
-    def eliminar_numero(self,posicion):# se le entrega un argumento llamado numero
-     try: # vamos a utilizar un try para fallos de un error
-        posicion = int (posicion) # aqui lo que hace es convertir lo que llega a un entero
+# se crea el metodo para borrar un numero de la lista
+    def eliminar_numero(self,posicion):
 
-        if 0 <= posicion < len(self.lista_numero):
-            eliminado = self.lista_numero.pop(posicion)
-            print (f" se elimino el numero en la {eliminado} en la posicion {posicion}")
-        else:
-            print(" la posicion no existe en lista.")# aca en llegado caso no estar el numero muentra este dicho mensaje 
+            try: # vamos a utilizar un try para fallos de un error
+                if 0 <= posicion < len(self.lista_numero):
+                    eliminado = self.lista_numero.pop(posicion)
+                    print (f" se elimino los numero: {eliminado}, en la posicion: {posicion}")
+                    
+                else:
+                    print(" la posicion no existe en lista.")# aca en llegado caso no estar el numero muentra este dicho mensaje 
+                    
+            except ValueError:
+                 print(" Error: Ingresa un número válido.")# al recibir dicho argumento y no se encuenre en la array va a decir dicho mensaje
+        
+# metodo remove
+    def remove_dato(self,result):#aqui llega como argumento 
+            try:
+                # se hace una condicional con un fallo de error 
+                if result in self.lista_numero:
+                    self.lista_numero.remove(result)#si los numeros esta en la losta los elimina 
+                    print(f"los numeros {result} fue eliminado.")
+                    
+                else:
+                    print(f"los numeros {result} no se encuentra en la lista.")#si no muestra el dicho mensaje
+                
+            except ValueError:
+                    print("Error: Ingresa un número válido.")
 
-        print(" Lista actualizada: ", self.lista_numero)# y se actualiza 
+#metodo index
+    def indeex(self,n_umeros):
+            try:
+                posicion=self.lista_numero.index(n_umeros)#creo la variable para almacenar la posicion del parametro que llega
+                print(f" el numero {n_umeros} esta en la posicion: {posicion}")
+            except ValueError:
+                print(f" error: el numero {n_umeros} no se encuentra en la lista ")
+                
+# metodo count
+    def coun(self,cantidad):
+            try:    
+                aux= self.lista_numero.count(cantidad)
+                if aux>0:
+                    print(f" el numero {cantidad} aparece {aux} veces en la lista")
+                else:
+                    print(f" el numero {cantidad} no se encuentra en la lista ")
+            except ValueError:
+                print(" error")
 
-     except ValueError:
-        print(" Error: Ingresa un número válido.")# al recibir dicho argumento y no se encuenre en la array va a decir dicho mensaje
-
-# se crea el metodo mostrar la lista 
-    def ver_Numero(self):
-       print(" lista completa :", self.lista_numero)
-
-    
+#metodo sort
+    def orden_lista(self):
+        self.lista_numero.sort()#se muestra la lista ordenada ascendente
+        print(" lista ordenada de menor a mayor: ",self.lista_numero)
+        
+#metodo reserve
+    def descedente_lista(self):
+        self.lista_numero.reverse()# se muestra la lista ordenadada de descendente
+        print(" lista ordenada de mayor a menor: ",self.lista_numero)
+                
+                
--- formulario.py
+++ formulario.py
@@ -14,8 +14,8 @@ class Calculadora:
 
 # se crea el medoto pedir numero
     def pedir_numero(self):
-        self.numero1=input(" ingresa el primer numero:")# se  pide el primer numero
-        self.numero2=input(" ingresa el segundo numero:")# se pide el segundo numero
+        self.numero1=int(input(" ingresa el primer numero:"))# se  pide el primer numero
+        self.numero2=int(input(" ingresa el segundo numero:"))# se pide el segundo numero
     
 # se crea el metodo almacenar_numero
     def almacenar_numero(self):
@@ -24,35 +24,115 @@ class Calculadora:
         
 # se crea el metodo incluir lista
     def incluir_lista(self):
-        numero3=input( "ingresa un numero: ")# se pide un tercer numero
-        numero4=input("ingrese otro numero: ")# y un cuarto numero
+        numero3=int(input( "ingresa un numero: "))# se pide un tercer numero
+        numero4=int(input("ingrese otro numero: "))# y un cuarto numero
         numeros_nuevos= [numero3,numero4] # aqui se crea una variable donde se alamacena dicho numeros
         self.oblista.guardar_numero(numeros_nuevos) # aqui se llama el metodo de la clase hija
 
 # se crea el metodo insertar dato
     def insertar_dato(self):
-        valor1 = input("Ingrese un numero: ")# se le pide un numero a el usuario  para añadirlo a la lista
-        valor2 = input("Ingresa otro numero: ")
+        valor1 = int(input("Ingrese un numero: "))# se le pide un numero a el usuario  para añadirlo a la lista
+        valor2 = int(input("Ingresa otro numero: "))
         agg_numeros= [valor1,valor2]
         posicion = int(input("En qué posición desea insertarlo: "))# se pide la posicion donde quiere posicionar en la lista 
         self.oblista.insertar_en_posicion(posicion,agg_numeros)# se llama al metodo de la clase y se le envia los argumentos
 
     # se crea el metodo eliminar dato
-    def eliminar_dato(self):
-        posicion = input(" ingrese la posicion del numero que deseas eliminar ")# se pide el numero a eliminar 
+    def eliminar_por_posicion(self):
+        posicion = int(input(" ingrese la posicion del numero que deseas eliminar "))# se pide el numero a eliminar 
         self.oblista.eliminar_numero(posicion)# llama el metodo de la clase y se envia el argumento
-        
-    def ver_numero(self):
-        self.oblista.ver_Numero()
-#menu 
     
+    def eliminar_por_valor(self):
+        numero1 = int(input("ingresa el primer numero a eliminar:"))
+        numero2 = int(input(" ingresa el segundo numero a eliminar:"))
+        result= [numero1,numero2]
+        self.oblista.remove_dato(result)
+    
+    def dato_por_posicion(self):
+        dato1=int(input("ingresa el primer dato a ver "))
+        dato2=int(input(" ingrese segundo dato a ver "))
+        n_umero=[dato1,dato2]
+        self.oblista.indeex(n_umero)
+    
+    def contar_lista(self):
+        num=int(input(" Ingrese el primer número a contar: "))
+        num1=int(input(" Ingrese el segundo número a contar: "))
+        cantidad=[num,num1]
+        self.oblista.coun(cantidad)
+        
+    def ordenar_numero(self):
+        self.oblista.orden_lista()
+    
+    def mayor_menor(self):
+        self.oblista.descedente_lista()
+    
+# se distancia el objecto principal
 
-# codigo principal
-objformulario = Calculadora()
-objformulario.pedir_numero()
-objformulario.almacenar_numero()
-objformulario.incluir_lista()
-objformulario.insertar_dato()
-objformulario.eliminar_dato()
-objformulario.ver_numero()
+formu=Calculadora()
+
+continuar= "s"
+
+# se crea un menu para el usuario
+while continuar== "s":   
+    print("\n==== BIENVENIDO USUARIO ====")
+    print("===== MENU DE ARRAYS ====")
+    print("1. Pedir dos numeros ")
+    print("2. Almacenar los numeros pedidos ")
+    print("3. Incluir nuevos numeros a la lista ")
+    print("4. Insertar numeros por posicion ")
+    print("5. Eliminar numero por posicion ")
+    print("6. Eliminar numero por valor ")
+    print("7. Buscar dato por posicion ")
+    print("8. Contar numeros de la lista ")
+    print("9. Ordenar lista de menor a mayor ")
+    print("10. Ordenar lista de mayor a menor ")
+    print("11. Ver lista actualizada ")
+    print("12. Salir ")
+
+#se crea una variable donde se va a guardar la opcion que eleji el usuario
+    opc= input(" selecione la opcion del (1-12):")
+#opciones para que el usuario dijite
+    match opc:
+        case "1":
+            formu.pedir_numero()
+        
+        case "2":
+            formu.almacenar_numero()
+        
+        case "3":
+            formu.incluir_lista()
+        
+        case "4":
+            formu.insertar_dato()
+        
+        case "5":
+            formu.eliminar_por_posicion()
+        
+        case "6":
+            formu.eliminar_por_valor()
+        
+        case "7":
+            formu.dato_por_posicion()
+        
+        case "8":
+            formu.contar_lista()
+        
+        case "9":
+            formu.ordenar_numero()
+        
+        case "10":
+            formu.mayor_menor()
+        
+        case "11":
+            print(" LISTA ACTUALIZADA:",formu.oblista.lista_numero)
+        
+        case "12":
+         print(" HASTA LUEGO ")
+         break
+    
+        case _:
+            print(" OPCION INVALIDA. ")
+
+    continuar = input("\n¿Desea realizar otra operación?(s:para seguir o n:para salir ): ").lower()
+    
 
